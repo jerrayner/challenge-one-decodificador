b123ab63f091ec2b0e968dc372ec3653681448bb
Jerrayne Rodrigues <jerrayner@gmail.com>
2022-08-08 13:03:49 -0300

    inserindo o botão de copiar no html e css, e fazendo ele funcionar no script

--- script/script.js
+++ script/script.js
@@ -1,63 +1,58 @@
-/*
-Resumo do desafio:
-Troca das vogais por essas strings:
-	a = ai
-	e = enter
-	i = imes
-	o = ober
-	u = ufat
+// variável auxiliar para receber o valor dos campos
+var output = "";
+var input = "";
 
-    Regras:
-	aceitar somente letras minúsculas
-	impedir caracteres especiais e letras com acentos
-	criptografar e decriptografar textos;
+// em vez de declarar dentro da função , declare fora da função para reaproveitar o código
+var textParaCriptografar = document.getElementById("texto");
+var textCriptografado = document.getElementById("output-resultado");
+// Função para criptografar:
+function criptografa() {
+  // input recebe o value do campo
+  input = textParaCriptografar.value;
 
-	Extra:
-	- Um botão que copie o texto criptografado/descriptografado para a área de transferência - ou seja, que tenha a mesma funcionalidade do ctrl+C ou da opção "copiar" do menu dos aplicativos.
-	 */
+  var palavraCriptografada = "";
 
-    // Função para criptografar:
+  for (let x = 0; x < input.length; x++) {
+    // console.log(textParaCriptografar[x]);
 
-    function criptografa(){
-        var textParaCriptografar = document.getElementById("texto").value;
-        var textCriptografado = document.getElementById("output-resultado");
-        var palavraCriptografada = "";
-
-        for(let x =0; x < textParaCriptografar.length; x++) {
-            console.log(textParaCriptografar[x]);
-
-        if (textParaCriptografar[x] === "a") {
-            palavraCriptografada += "ai"
-        } else if (textParaCriptografar[x] === "e") {
-            palavraCriptografada += "enter"
-        } else if (textParaCriptografar[x] === "i") {
-            palavraCriptografada += "imes"
-        } else if (textParaCriptografar[x] === "o") {
-            palavraCriptografada += "ober"
-        } else if (textParaCriptografar[x] === "u") {
-            palavraCriptografada += "ufat"
-        }
-		else { 
-            palavraCriptografada += textParaCriptografar[x]
-        }
-        console.log(palavraCriptografada);
-        textCriptografado.value = palavraCriptografada;
-        }
-    }   
-
-    function descriptografa() {
-    
-        var textoCriptografado = document.getElementById("output-resultado").value;
-        var textDescriptografado = document.getElementById("texto");
-
-    
-        var descriptografado = textoCriptografado.replace(/ai/g, 'a')
-        descriptografado = descriptografado.replace(/enter/g, 'e')
-        descriptografado = descriptografado.replace(/imes/g, 'i')
-        descriptografado = descriptografado.replace(/ober/g, 'o')
-        descriptografado = descriptografado.replace(/ufat/g, 'u')
-    
-        textDescriptografado.value = descriptografado;
-    
+    if (input[x] === "a") {
+      palavraCriptografada += "ai";
+    } else if (input[x] === "e") {
+      palavraCriptografada += "enter";
+    } else if (input[x] === "i") {
+      palavraCriptografada += "imes";
+    } else if (input[x] === "o") {
+      palavraCriptografada += "ober";
+    } else if (input[x] === "u") {
+      palavraCriptografada += "ufat";
+    } else {
+      palavraCriptografada += input[x];
     }
+    // console.log(palavraCriptografada);
+    textCriptografado.innerHTML = palavraCriptografada;
+  }
+}
+function descriptografa() {
+  // fiz uma variável auxiliar receber o value
+  output = textParaCriptografar.value;
 
+  // reduzi um pouco o código
+  var descriptografado = output
+    .replaceAll(/ai/g, "a")
+    .replaceAll(/enter/g, "e")
+    .replaceAll(/imes/g, "i")
+    .replaceAll(/ober/g, "o")
+    .replaceAll(/ufat/g, "u");
+  console.log(descriptografado);
+
+  textCriptografado.innerHTML = descriptografado;
+}
+
+/* Função do botao de copiar */
+function copiarTexto() {
+    let textoCopiado = document.getElementById("output-resultado");
+        textoCopiado.select();
+        textoCopiado.setSelectionRange(0, 99999)
+        document.execCommand("copy");
+        alert("O texto é: " + textoCopiado.value);
+}
